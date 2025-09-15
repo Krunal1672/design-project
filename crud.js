ade556b7271f8c2a4e22ba80e0996f13a5990d76
Krunal1672 <sanghanikrunal2005@gmail.com>
2025-09-15 18:44:45 +0530

    Add files via upload

---
+++ crud.js
@@ -0,0 +1,91 @@
+let allData = []
+const saveData = ()=>{
+    let nm = document.getElementById('name').value
+    let age = document.frm.age.value
+    let sal = document.frm.salary.value
+    let id = document.frm.id.value
+    let data = JSON.parse(localStorage.getItem('userinfo'))
+    let len = (data!=null)? data.length+1 : 1;
+
+    if (nm === "" || age === "" || sal === "") {
+        alert("Please fill in all fields before saving!")
+        return
+    }
+    
+    if(id!=''){
+        //update
+        allData = data.map((i)=>{
+            if(i.id == id){
+                i.name = nm
+                i.age = age
+                i.salary = sal
+            }
+            return i
+        })
+    }else{
+        //insert
+         let obj = {
+            id:len,
+            name:nm,
+            age:age,
+            salary:sal
+        }
+        allData.push(obj)
+    }
+  
+  
+    document.frm.reset()
+    document.frm.id.value = ''
+    localStorage.setItem("userinfo",JSON.stringify(allData))
+    dispData()
+}
+
+const dispData = ()=>{
+    let data = JSON.parse(localStorage.getItem('userinfo'))
+    let tr = ''
+    data.map((i)=>{
+        tr += `<tr>
+                    <td>${i.id}</td>
+                    <td>${i.name}</td>
+                    <td>${i.age}</td>
+                    <td>${i.salary}</td>
+                    <td>
+                        <button onclick="editData(${i.id})">Edit</button>
+                        <button onclick="delData(${i.id})">Delete</button>
+                    </td>
+            </tr>`       
+    })
+    document.getElementById('alldata').innerHTML = tr
+}
+//delete
+const delData = (id)=>{
+    let data = JSON.parse(localStorage.getItem('userinfo'))
+    //1 2  4 5 == id=3
+    let res = data.filter((i)=>{
+            return i.id != id
+    })
+    let j=1
+    let newdata = res.map((i)=>{
+            i.id = j++
+            return i
+    })
+    localStorage.setItem("userinfo",JSON.stringify(newdata))
+    allData = newdata
+    console.log(allData);
+    
+    dispData()
+}
+//edit
+const editData =(id)=>{
+    let data = JSON.parse(localStorage.getItem('userinfo'))
+    let res =data.find((i)=>{
+        return i.id == id
+    })
+    console.log(res);
+    document.getElementById('name').value= res.name
+    document.frm.age.value = res.age
+    document.frm.salary.value = res.salary
+    document.frm.id.value= id
+    
+}
+dispData()
